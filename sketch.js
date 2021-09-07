7d31cf4f810d8c8b02fcbc33d49ff3fcbc67b587
fmehmetun <fmehmetun3@gmail.com>
2021-09-07 10:32:19 +0300

    sketch.js -> Being able to interact with the network, added buttons for interaction

--- sketch.js
+++ sketch.js
@@ -1,53 +1,62 @@
+//// Data
 let n_samples = 16;
 let n_features = 16;
 let n_targets = 2;
 
-// Data related
 // let dataFrame = {};
 // initData = () => {
 // 	dataFrame.X = tf.randomNormal([n_samples, n_features]);
 // 	dataFrame.y = tf.randomNormal([n_samples, n_targets]);
 // };
 
-// Neural network related
-let nnCanvas;
-let nnCanvasRatio = {x: 1.0, y: 1.0};
-let nnCreateCanvas = () => {
-	return createGraphics(
-		(windowWidth*nnCanvasRatio.x),
-		(windowHeight*nnCanvasRatio.y)
-	);
-};
-
+//// Neural network
+// Our main neural network model
 let nn;
-let nnHiddenLayersStructure = [
-	// Hidden layers
-	{class: tf.layers.dense, args: {"units": 16, "activation": "sigmoid", "useBias": true}},
-	{class: tf.layers.dense, args: {"units": 12, "activation": "sigmoid", "useBias": true}},
-	{class: tf.layers.dense, args: {"units": 8, "activation": "sigmoid", "useBias": true}},
-	{class: tf.layers.dense, args: {"units": 4, "activation": "sigmoid", "useBias": true}},
-];
+
+// Creates a dense layer config object with given values
+let createDenseLayerConfig = (units=null, useBias=null, activation=null) => (
+	{
+		class: tf.layers.dense,
+		args: {
+			// Min 4, max 15 neurons, if not specified
+			units: ((units===null) ? getRandomInt(4, 16) : units),
+			// Randomly choose if use bias or not, if not specified
+			useBias: ((useBias===null) ? (getRandomInt(0, 2)===1) : useBias),
+			// No activation, if not specified
+			activation: activation
+		}
+	}
+);
+
+// Specifies our neural network structure (layers, losses etc.)
 let nnStructure = {
-	layers: [
-		// Input layer
-		{class: tf.layers.inputLayer, args: {inputShape: [n_features]}},
+	// Input layer
+	inputLayer: {
+		class: tf.layers.inputLayer,
+		args: {inputShape: [n_features]}
+	},
 
-		// Hidden layer(s)
-		...nnHiddenLayersStructure,
+	// Hidden layers (create them randomly at start: min 1, max 5 layers)
+	hiddenLayers: [...Array(getRandomInt(1, 6)).keys()].map(layer => (
+		createDenseLayerConfig()
+	)),
 
-		// Output layer
-		{
-			class: tf.layers.dense,
-			args: {
-				"units": n_targets,
+	// Output layer
+	outputLayer: createDenseLayerConfig(
+		// units
+		n_targets,
 
-				// Regression
-				"activation": null,
-				// Classification
-				// "activation": "sigmoid"
-			}
-		},
-	],
+		// useBias
+		true,
+
+		// activation
+		// Regression
+		null,
+		// Classification
+		// activation: "sigmoid",
+	),
+
+	// Compile arguments (optimizer, loss)
 	compileArgs: {
 		optimizer: "sgd",
 
@@ -57,6 +66,18 @@ let nnStructure = {
 		// loss: "categoricalCrossentropy"
 	},
 };
+
+// Canvas which our neural network will be drawn
+let nnCanvas;
+let nnCanvasRatio = {x: 1.0, y: 1.0};
+let nnCreateCanvas = () => {
+	return createGraphics(
+		(windowWidth*nnCanvasRatio.x),
+		(windowHeight*nnCanvasRatio.y)
+	);
+};
+
+// Builds neural network object at tf.js side with structure config, returns it
 buildNeuralNetwork = () => {
 	// Build NN sequentially with our custom class
 	__nn__ = new SequentialNeuralNetwork(
@@ -64,21 +85,67 @@ buildNeuralNetwork = () => {
 		sequentialArgs={},
 	);
 
-	// Add layers
-	nnStructure.layers.forEach(layer => {
+	// Put all layer configs in a list, add each of them to the model
+	[
+		nnStructure.inputLayer,
+		...nnStructure.hiddenLayers,
+		nnStructure.outputLayer
+	].forEach(layer => {
 		__nn__.add(
 			layer.class(layer.args)
 		);
 	});
 
-	// Compile the model
+	// Compile the model with args
 	__nn__.compile(nnStructure.compileArgs);
 
 	return __nn__;
 };
 
+// Initialize GUI components
+initializeGUI = () => {
+	// Predict
+	let predictButton;
+	predictButton = createButton("predict");
+	predictButton.position(0, 0);
+	predictButton.mousePressed(args => {
+		nn.predict(
+			tf.randomNormal([1, n_features])
+		);
+	});
+
+	// Add hidden layer button
+	let addHiddenLayerButton;
+	addHiddenLayerButton = createButton("add hidden layer");
+	addHiddenLayerButton.position(150, 0);
+	addHiddenLayerButton.mousePressed(args => {
+		// Add one layer to config & rebuild neural network
+		nnStructure.hiddenLayers.push(createDenseLayerConfig());
+		nn = buildNeuralNetwork();
+	});
+
+	// Remove hidden layers button
+	let removeHiddenLayersButton;
+	removeHiddenLayersButton = createButton("remove hidden layers");
+	removeHiddenLayersButton.position(300, 0);
+	removeHiddenLayersButton.mousePressed(args => {
+		// Reset configs & rebuild neural network
+		nnStructure.hiddenLayers = [];
+		nn = buildNeuralNetwork();
+	});
+
+	// Reset neural network button
+	let resetnnButton;
+	resetnnButton = createButton("reset nn");
+	resetnnButton.position(450, 0);
+	resetnnButton.mousePressed(args => {
+		// Reset configs & rebuild neural network
+		nnStructure.hiddenLayers = [...Array(getRandomInt(1, 6)).keys()].map(layer => (createDenseLayerConfig()));
+		nn = buildNeuralNetwork();
+	});
+};
+
 // Setup
-// DOM objects
 setup = () => {
 	// Create main canvas
 	createCanvas(windowWidth, windowHeight);
@@ -86,14 +153,8 @@ setup = () => {
 	// Create the canvas which will neural network be drawn
 	nnCanvas = nnCreateCanvas();
 
-	let button;
-	button = createButton("predict");
-	button.position(0, 0);
-	button.mousePressed(args => {
-		nn.predict(
-			tf.randomNormal([1, n_features])
-		);
-	});
+	// Initialize GUI components
+	initializeGUI();
 
 	// Build neural network
 	nn = buildNeuralNetwork();
@@ -108,7 +169,7 @@ draw = () => {
 	// Draw the whole network on the given canvas
 	nn.draw(
 		nnCanvas,
-		{"gapRateX": 0.8, "gapRateY": 0.8}
+		{gapRateX: 0.8, gapRateY: 0.8}
 	);
 
 	// Draw the network canvas to the main canvas 
--- utils.js
+++ utils.js
@@ -1,7 +1,16 @@
+// Linear interpolation
 const lerp = (value, target, amount) => {
 	return (value + ((target - value) * amount));
 };
 
+// Random int
+getRandomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Animation related functions
 class AnimationUtils{
 	// Exponential
 	static easeOutExpo = (x) => {
